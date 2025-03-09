b7469c6adcec27c65f0c27e0681cc83700f56338
Erik Schierboom <erik_schierboom@hotmail.com>
2025-03-09 11:47:35 +0100

    r: simplify

--- 3-languages-3-paradigms/2025-ndc-oslo/r.R
+++ 3-languages-3-paradigms/2025-ndc-oslo/r.R
@@ -53,19 +53,4 @@ tbl[, 2]
 tbl + 1
 
 # Example: hamming distance
-#
-# [C#]
-# var length = 0;
-#
-# for (var i = 0; i < strand1.Length; i++)
-# {
-#    if (strand1[i] != strand2[i])
-#    {
-#        length++;
-#    }
-# }
-#
-# return length;
-#
-# [R]
 # sum(strand1 != strand2)
