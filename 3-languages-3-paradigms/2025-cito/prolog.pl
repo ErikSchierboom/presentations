669374197e7f714f261e69c5e45626e53ee7a835
Erik Schierboom <erik_schierboom@hotmail.com>
2025-03-23 08:48:10 +0100

    Update prolog

--- 3-languages-3-paradigms/2025-cito/prolog.pl
+++ 3-languages-3-paradigms/2025-cito/prolog.pl
@@ -29,36 +29,38 @@ non_dutch_medalist(Name) :- medalist(Name), \+ dutch(Name). % \+ = NOT
 % european(Name).
 % non_dutch_medalist(Name).
 
-% Unification
-1 = 1. % true (two identical values unify)
-a = X. % true (something and a variable unify)
-foo(X, b) = foo(a, Y). % true (two compound terms of same arity and variables unify)
-foo(a, b) = foo(X, X). % false (X can't be unified to both a and b)
-[1, 2, Z] = [1, Y, 3]. % true (two lists of same length with unifying elements)
-[1, 2, 3] = [X | Y].   % true (non-empty list unifies with head and tail)
-
 % Bi-directional predicates
-string_chars("Hi", Chars).
-string_chars(String, [h, e, l, l, o]).
+string_chars("hey", Chars).
+string_chars(String, [h, e, y]).
+string_chars("hi", [h, e, y]).
 
 append([1, 2], [3], Appended).
 append([1, 2], Suffix, [1, 2, 3]).
 append(Prefix, Suffix, [1, 2, 3]).
 
 % CLP(FD)
+
+% SEND + MORE = MONEY
 :- use_module(library(clpfd)).
 
-% Regular arithmetic is not bidirectional
-% X =:= 2. % ERROR: Arguments are not sufficiently instantiated
-% 2 is X.  % ERROR: Arguments are not sufficiently instantiated
+solve(Solution) :-
+    Vars = [S, E, N, D, M, O, R, Y],
+    Vars ins 0..9,
+    all_different(Vars),
 
-% CLP(FD) is bidirectional
-1 #< X, 3 #> X.
+    S #\= 0, M #\= 0,  % No leading zeros
 
-% CLP(FD) supports ranges
-X #> 3, X #=< 10.
+    1000*S + 100*E + 10*N + D +
+    1000*M + 100*O + 10*R + E #=
+    10000*M + 1000*O + 100*N + 10*E + Y,
+
+    label(Vars),
+    Solution = [s-S, e-E, n-N, d-D, m-M, o-O, r-R, y-Y].
+
+% solve(Solution).
 
 % DCG
+
 :- use_module(library(dcg/basics)).
 
 degrees --> "°".
