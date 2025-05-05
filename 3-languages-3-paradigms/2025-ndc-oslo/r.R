6099c93e56452d59dc87939160fe3840e3ddefd6
Erik Schierboom <erik_schierboom@hotmail.com>
2025-05-05 08:56:46 +0200

    r: fix power operator

--- 3-languages-3-paradigms/2025-ndc-oslo/r.R
+++ 3-languages-3-paradigms/2025-ndc-oslo/r.R
@@ -2,26 +2,26 @@
 1 + 2
 
 # Vectors (homogenous: same type)
-c()
 c(1, 2, 3)
-c(TRUE, FALSE, TRUE)
+c(1, 2, 3) == 1:3 # Range syntax
 
 # Scalars are vectors!
 length(c(1))
 length(1)
-1 == c(1)
-
-# Vectors using range syntax
-1:3
+1 == c(1) # Implication: 1 + 2 was using vectors
 
 # Numerical operations on vectors
 1:3 + 5
 1:3 * 4:6
 1:3 - 4:5 # Warning due to different lengths
-(1:n)^2
+(1:3)^2
+
+# Logical operations on vectors
+1:3 > 2
+1:3 == c(3, 2, 1)
 
 # Indexing is 1-based
-(4:6)[1]
+(4:6)[1] # Implication: can use vectors as indices
 
 # Index with a numeric vector
 (4:6)[c(1)]
@@ -32,14 +32,14 @@ length(1)
 (1:3)[c(TRUE, FALSE, TRUE)]
 
 # Combine logical indexing with logical vector
-(1:10)[1:10 > 5]
-(1:10)[1:10 %% 3 == 0]
+numbers <- 1:10
+numbers[numbers > 5]
+numbers[numbers %% 3 == 0]
 
 # Update vectors using indexing
-numbers <- 1:10
 numbers[2] <- 4
+numbers[c(1, 10)] <- 3
 numbers[numbers %% 2 == 0] <- 0
-numbers[numbers > 5] <- numbers[numbers > 5] + 1
 
 # Multi-dimensional collections (data frames)
 tbl <- read.table(text = "1 2 3\n4 5 6")
@@ -49,3 +49,4 @@ tbl[, 2]
 
 # Operations work on multi-dimensional collections
 tbl + 1
+tbl[2, ] <- tbl[2, ] + 1
