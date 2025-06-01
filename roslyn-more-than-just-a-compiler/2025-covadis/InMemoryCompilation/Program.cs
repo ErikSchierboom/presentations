3de6df7619a0e64c74215c2972b19d3fcf107eef
Erik Schierboom <erik_schierboom@hotmail.com>
2025-06-01 09:42:59 +0200

    Add in-memory compilation

--- roslyn-more-than-just-a-compiler/2025-covadis/InMemoryCompilation/Program.cs
+++ roslyn-more-than-just-a-compiler/2025-covadis/InMemoryCompilation/Program.cs
@@ -1,26 +1,39 @@
-var builder = WebApplication.CreateBuilder(args);
+﻿using System.Reflection;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
 
-// Add services to the container.
-builder.Services.AddRazorPages();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+const string greeterClassSource = @"
+public class Greeter
 {
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    public string Greet(string name)
+    {
+        return $""Hello, {name}!"";
+    }
+}
+";
+
+var syntaxTree = CSharpSyntaxTree.ParseText(greeterClassSource);
+
+const string assemblyName = "GreeterAssembly";
+var compilation = CSharpCompilation.Create(
+    assemblyName,
+    syntaxTrees: [syntaxTree],
+    references: [MetadataReference.CreateFromFile(typeof(object).Assembly.Location)],
+    options: new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary));
+
+var stream = new MemoryStream();
+var emitResult = compilation.Emit(stream);
+if (!emitResult.Success)
+{
+    foreach (var diagnostic in emitResult.Diagnostics)
+        Console.WriteLine(diagnostic);
+
+    return;
 }
 
-app.UseHttpsRedirection();
+var assembly = Assembly.Load(stream.ToArray());
+var greeterClass = assembly.GetType("Greeter")!;
+var greeterInstance = Activator.CreateInstance(greeterClass);
+var greeting = greeterClass.GetMethod("Greet")!.Invoke(greeterInstance, ["World"]);
 
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapStaticAssets();
-app.MapRazorPages()
-    .WithStaticAssets();
-
-app.Run();
+Console.WriteLine(greeting);
