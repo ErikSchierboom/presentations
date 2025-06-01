9a0db1baeece70cbc8e046da1f1c731266cd53c2
Erik Schierboom <erik_schierboom@hotmail.com>
2025-06-01 20:35:23 +0200

    Final: analyzer

--- roslyn-more-than-just-a-compiler/2025-covadis/Analyzer/ExponentNotationCodeFixProvider.cs
+++ roslyn-more-than-just-a-compiler/2025-covadis/Analyzer/ExponentNotationCodeFixProvider.cs
@@ -1,16 +1,22 @@
 using System.Collections.Immutable;
 using System.Composition;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CodeActions;
 using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Text;
 
 namespace Analyzer;
 
 [ExportCodeFixProvider(LanguageNames.CSharp, Name = nameof(ExponentNotationCodeFixProvider)), Shared]
 public class ExponentNotationCodeFixProvider : CodeFixProvider
 {
+    private const string Title = "Convert to exponent notation";
+    
     public sealed override ImmutableArray<string> FixableDiagnosticIds { get; } =
         [ExponentNotationAnalyzer.DiagnosticId];
 
@@ -18,7 +24,22 @@ public class ExponentNotationCodeFixProvider : CodeFixProvider
 
     public sealed override async Task RegisterCodeFixesAsync(CodeFixContext context)
     {
-        // TODO: find the literal expression syntax
-        // TODO: register code fix
+        var diagnostic = context.Diagnostics.Single();
+        var codeAction = CodeAction.Create(
+            Title,
+            createChangedSolution: ct => UseExponentNotation(context.Document, diagnostic.Location.SourceSpan, ct),
+            Title);
+        context.RegisterCodeFix(codeAction, diagnostic);
+    }
+
+    private static async Task<Solution> UseExponentNotation(Document document, TextSpan textSpan, CancellationToken cancellationToken)
+    {
+        var root = await document.GetSyntaxRootAsync(cancellationToken);
+        var literalExpression = (LiteralExpressionSyntax)root.FindNode(textSpan, getInnermostNodeForTie: true);
+        
+        var newLiteralExpression = literalExpression.WithToken(SyntaxFactory.Literal("1e9", 1e9));
+        var newRoot = root.ReplaceNode(literalExpression, newLiteralExpression);
+        var newDocument = document.WithSyntaxRoot(newRoot);
+        return newDocument.Project.Solution;
     }
 }
