0879b60335bad9a3ea29f9591b84d3df4c83214a
Erik Schierboom <erik_schierboom@hotmail.com>
2025-06-01 19:47:25 +0200

    Final renaming

--- roslyn-more-than-just-a-compiler/2025-covadis/Renaming/Program.cs
+++ roslyn-more-than-just-a-compiler/2025-covadis/Renaming/Program.cs
@@ -1,4 +1,41 @@
-﻿var solutionFilePath = Path.GetFullPath("../../../../RoslynMoreThanJustACompiler.sln");
+﻿using Humanizer;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Editing;
+using Microsoft.CodeAnalysis.MSBuild;
+using Microsoft.CodeAnalysis.Rename;
+
+var solutionFilePath = Path.GetFullPath("../../../../RoslynMoreThanJustACompiler.sln");
+var workspace = MSBuildWorkspace.Create();
+var solution = await workspace.OpenSolutionAsync(solutionFilePath);
+
+foreach (var project in solution.Projects)
+{
+    var compilation = await project.GetCompilationAsync();
+    var factAttribute = compilation.GetTypeByMetadataName("Xunit.FactAttribute")!;
+
+    foreach (var document in project.Documents)
+    {
+        var documentEditor = await DocumentEditor.CreateAsync(document);
+        var methods = documentEditor.OriginalRoot
+            .DescendantNodes()
+            .OfType<MethodDeclarationSyntax>();
+
+        foreach (var method in methods)
+        {
+            var methodSymbol = (IMethodSymbol)documentEditor.SemanticModel.GetDeclaredSymbol(method)!;
+            var isTestMethod = methodSymbol.GetAttributes()
+                .Any(attribute => attribute.AttributeClass.Equals(factAttribute, SymbolEqualityComparer.Default));
+            if (isTestMethod)
+            {
+                solution = await Renamer.RenameSymbolAsync(solution, methodSymbol, new SymbolRenameOptions(),
+                    methodSymbol.Name.Pascalize());
+            }
+        }
+    }
+}
+
+workspace.TryApplyChanges(solution);
 
 Console.WriteLine("Renamed");
 
