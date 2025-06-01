c8bf5eb37a26e916b1a6de5fed493b5c63e83b54
Erik Schierboom <erik_schierboom@hotmail.com>
2025-06-01 19:41:26 +0200

    Final rewrite

--- roslyn-more-than-just-a-compiler/2025-covadis/Rewriting/Program.cs
+++ roslyn-more-than-just-a-compiler/2025-covadis/Rewriting/Program.cs
@@ -1,13 +1,37 @@
-﻿using Microsoft.CodeAnalysis.CSharp;
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 var sourceCodeFilePath = Path.GetFullPath("../../../../Rewriting.Example/Gigasecond.cs");
 var sourceCode = File.ReadAllText(sourceCodeFilePath);
 
 var syntaxTree = CSharpSyntaxTree.ParseText(sourceCode);
 var root = await syntaxTree.GetRootAsync();
+root = new RemoveEmptyStatements().Visit(root);
+root = new UseExponentNotation().Visit(root);
+root = root.NormalizeWhitespace();
+
+File.WriteAllText(sourceCodeFilePath, root.ToFullString());
 
 Console.WriteLine("Rewritten");
 
-// TODO: remove empty statements
-// TODO: use exponent notation
-// TODO: use expression-bodied members
+internal class RemoveEmptyStatements : CSharpSyntaxRewriter
+{
+    public override SyntaxNode? VisitEmptyStatement(EmptyStatementSyntax node)
+    {
+        return null;
+    }
+}
+
+internal class UseExponentNotation : CSharpSyntaxRewriter
+{
+    public override SyntaxToken VisitToken(SyntaxToken token)
+    {
+        if (token.IsKind(SyntaxKind.NumericLiteralToken) &&
+            token.Value is 1_000_000_000 &&
+            token.Text != "1e9")
+            return SyntaxFactory.Literal("1e9", 1_000_000_000);
+            
+        return base.VisitToken(token);
+    }
+}
