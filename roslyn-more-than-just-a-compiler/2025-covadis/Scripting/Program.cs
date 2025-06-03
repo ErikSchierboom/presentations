19f5236915120f763498133ad400925f37cf6e58
Erik Schierboom <erik_schierboom@hotmail.com>
2025-06-03 19:28:18 +0200

    Revert scripting

--- roslyn-more-than-just-a-compiler/2025-covadis/Scripting/Program.cs
+++ roslyn-more-than-just-a-compiler/2025-covadis/Scripting/Program.cs
@@ -1,19 +1,4 @@
 // TODO: run code from command-line
 // TODO: run code from command-line using previous state
 
-using Microsoft.CodeAnalysis.CSharp.Scripting;
-
-Console.Write("> ");
-var sourceCode = Console.ReadLine();
-var state = await CSharpScript.RunAsync(sourceCode);
-Console.WriteLine(state.ReturnValue);
-
-while (true)
-{
-    Console.Write("> ");
-    sourceCode = Console.ReadLine();
-    state = await state.ContinueWithAsync(sourceCode);
-    Console.WriteLine(state.ReturnValue);
-}
-
 Console.WriteLine("Done");
