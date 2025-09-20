7a97e66eefe7f95bf78619a1b5a61b1ee6178e36
Erik Schierboom <erik_schierboom@hotmail.com>
2025-09-20 10:30:52 +0200

    Fix naming

--- roslyn-more-than-just-a-compiler/2025-cito/Rewriting.Example/Gigasecond.cs
+++ roslyn-more-than-just-a-compiler/2025-cito/Rewriting.Example/Gigasecond.cs
@@ -6,7 +6,13 @@ namespace Rewriting.Example
     {
         public static DateTime Add(DateTime birthDate)
         {
+            Log_Date_Time(birthDate);
             return birthDate.AddSeconds(1e9);
+        }
+
+        private static void Log_Date_Time(DateTime dateTime)
+        {
+            Console.WriteLine(dateTime);
         }
     }
 }
