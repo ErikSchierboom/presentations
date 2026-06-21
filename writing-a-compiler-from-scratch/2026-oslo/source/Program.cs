4ba48c19227f4b42ed3c650eea6ee3ede6ca1290
Erik Schierboom <erik_schierboom@hotmail.com>
2026-06-21 10:42:47 +0200

    Prepare for parsing operator precedence

--- writing-a-compiler-from-scratch/2026-oslo/source/Program.cs
+++ writing-a-compiler-from-scratch/2026-oslo/source/Program.cs
@@ -1,6 +1,6 @@
 ﻿const string code = """
                     var x = 3;
-                    var result = 1 + 2 * x;
+                    var result = 1 * 2 + x;
                     result + 10
                     """;
 
