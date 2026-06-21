f1588bbf3536100613a109d538f9ab0bc42f79f3
Erik Schierboom <erik_schierboom@hotmail.com>
2026-06-21 16:01:57 +0200

    Fix code

--- writing-a-compiler-from-scratch/2026-oslo/source/Program.cs
+++ writing-a-compiler-from-scratch/2026-oslo/source/Program.cs
@@ -2,8 +2,8 @@
 
 const string code = """
                     var x = 3;
-                    var result = 1 + 2 * x;
-                    result + 10
+                    var result = 1 * 2 + x;
+                    result + 10;
                     """;
 
 var tokens = new Scanner(code).Scan();
