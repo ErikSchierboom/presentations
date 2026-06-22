bbf9d79a8734ff99f6741a7312616a919d614eee
Erik Schierboom <erik_schierboom@hotmail.com>
2026-06-22 09:24:08 +0200

    Add Cito presentation

---
+++ writing-a-compiler-from-scratch/2026-cito/source/Program.cs
@@ -0,0 +1,37 @@
+﻿const string code = """
+                    var x = 3;
+                    var result = 1 * 2 + x;
+                    result + 10;
+                    """;
+
+var tokens = new Scanner(code).Scan();
+foreach (var token in tokens)
+{
+    Console.WriteLine(token);
+}
+
+public record Token(TokenType Type, string Text);
+
+public enum TokenType
+{
+    Equal,
+    Plus,
+    Star,
+    Semicolon,
+    Num,
+    Ident,
+    Var,
+    Eof
+}
+
+public class Scanner(string source)
+{
+    public List<Token> Scan()
+    {
+        var tokens = new List<Token>();
+
+        // TODO: parse tokens
+        
+        return tokens;
+    }
+}
