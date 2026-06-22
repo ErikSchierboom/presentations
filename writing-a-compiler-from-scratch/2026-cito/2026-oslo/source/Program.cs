80d359258833d3946152c2cb08052721db33faeb
Erik Schierboom <erik_schierboom@hotmail.com>
2026-06-22 09:25:25 +0200

    Add Cito presentation

---
+++ writing-a-compiler-from-scratch/2026-cito/2026-oslo/source/Program.cs
@@ -0,0 +1,44 @@
+﻿using BoomScript;
+
+const string code = """
+                    var x = 3;
+                    var result = 1 * 2 + x;
+                    result + 10;
+                    """;
+
+var tokens = new Scanner(code).Scan();
+var tree = new Parser(tokens).Parse();
+var instructions = new Compiler(tree).Compile();
+var result = new VirtualMachine(instructions).Run();
+Console.WriteLine(result);
+
+public abstract record Instruction;
+public record LoadIntInstruction(int Value) : Instruction;
+public record LoadVarInstruction(int Index) : Instruction;
+public record StoreVarInstruction(int Index) : Instruction;
+public record AddInstruction : Instruction;
+public record MulInstruction : Instruction;
+
+public class Compiler(Tree tree)
+{
+    private readonly List<Instruction> _instructions = new();
+    private readonly Dictionary<string, int> _variableToIndex = new(capacity: 256);
+
+    public List<Instruction> Compile()
+    {
+        // TODO: implement compiler
+        
+        return _instructions;
+    }
+}
+
+public class VirtualMachine(List<Instruction> instructions)
+{
+    public Stack<int> Stack { get; } = new();
+    public int[] Variables { get; } = new int[256];
+    
+    public int Run()
+    {   
+        throw new NotImplementedException();
+    }
+}
