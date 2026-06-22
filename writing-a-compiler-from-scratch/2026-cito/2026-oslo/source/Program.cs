834234c02ed9e51ab5e5533cc934230cb23b750e
Erik Schierboom <erik_schierboom@hotmail.com>
2026-06-22 09:25:09 +0200

    Add Cito presentation

---
+++ writing-a-compiler-from-scratch/2026-cito/2026-oslo/source/Program.cs
@@ -0,0 +1,42 @@
+﻿using BoomScript;
+
+const string code = """
+                    var x = 3;
+                    var result = 1 * 2 + x;
+                    result + 10;
+                    """;
+
+var tokens = new Scanner(code).Scan();
+var tree = new Parser(tokens).Parse();
+foreach (var statement in tree.Statements)
+{
+    Console.WriteLine(statement);
+}
+
+public record Tree(List<Statement> Statements);
+
+public abstract record Statement;
+public record AssignmentStatement(string VariableName, Expression Value) : Statement;
+public record ExpressionStatement(Expression Expression) : Statement;
+
+public abstract record Expression;
+public record NumberLiteralExpression(int Value) : Expression;
+public record BinaryExpression(Expression Left, TokenType Operator, Expression Right) : Expression;
+public record VariableExpression(string Name) : Expression;
+
+public class Parser(List<Token> tokens)
+{
+    private int _position = 0;
+    
+    public Tree Parse()
+    {
+        var statements = new List<Statement>();
+        
+        // TODO: parse statements
+        
+        return new Tree(statements);
+    }
+
+    private Token Current => tokens[_position];
+    private Token Previous => tokens[_position - 1];
+}
---
+++ writing-a-compiler-from-scratch/2026-cito/2026-oslo/source/Scanner.cs
@@ -0,0 +1,77 @@
+﻿namespace BoomScript;
+
+public record Token(TokenType Type, string Text);
+
+public enum TokenType
+{
+    Equal,
+    Plus,
+    Star,
+    Semicolon,
+    Num,
+    Ident,
+    Var,
+    Eof
+}
+
+public class Scanner(string source)
+{
+    public List<Token> Scan()
+    {
+        var tokens = new List<Token>();
+        var position = 0;
+
+        while (position < source.Length)
+        {
+            var character = source[position];
+            switch (character)
+            {
+                case ' ' or '\r' or '\n':
+                    position++;
+                    continue;
+                case '+':
+                    tokens.Add(new Token(TokenType.Plus, "+"));
+                    position++;
+                    break;
+                case '*':
+                    tokens.Add(new Token(TokenType.Star, "*"));
+                    position++;
+                    break;
+                case ';':
+                    tokens.Add(new Token(TokenType.Semicolon, ";"));
+                    position++;
+                    break;
+                case '=':
+                    tokens.Add(new Token(TokenType.Equal, "="));
+                    position++;
+                    break;
+                case >= '0' and <= '9':
+                    var integerStart = position;
+                    
+                    while (position < source.Length && source[position] is >= '0' and <= '9')
+                        position++;
+                    
+                    tokens.Add(new Token(TokenType.Num, source[integerStart..position]));
+                    break;
+                case >= 'a' and <= 'z':
+                    var identifierStart = position;
+                    
+                    while (position < source.Length && source[position] is >= 'a' and <= 'z')
+                        position++;
+
+                    var lexeme = source[identifierStart..position];
+                    if (lexeme == "var")
+                        tokens.Add(new Token(TokenType.Var, lexeme));
+                    else
+                        tokens.Add(new Token(TokenType.Ident, lexeme));
+                    break;
+                default:
+                    throw new Exception($"Unexpected character: {character}.");
+            }
+        }
+        
+        tokens.Add(new Token(TokenType.Eof, ""));
+
+        return tokens;
+    }
+}
